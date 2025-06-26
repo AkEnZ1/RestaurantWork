fde6b8a26ce12498fef44975308809c4e0418f69
Nastya <chicha10.06@mail.ru>
2025-06-27 01:54:28 +0700

    добавили форму регистрации

---
+++ Restaurant/Restaurant/RegisterForm.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace Restaurant
+{
+    partial class RegisterForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.pass_reg = new System.Windows.Forms.TextBox();
+            this.log_reg = new System.Windows.Forms.TextBox();
+            this.btnRegister = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.pass_reg);
+            this.panel1.Controls.Add(this.log_reg);
+            this.panel1.Controls.Add(this.btnRegister);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(203, 65);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(416, 286);
+            this.panel1.TabIndex = 0;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(52, 151);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(48, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Пароль:";
+            // 
+            // pass_reg
+            // 
+            this.pass_reg.Location = new System.Drawing.Point(52, 170);
+            this.pass_reg.Name = "pass_reg";
+            this.pass_reg.Size = new System.Drawing.Size(100, 20);
+            this.pass_reg.TabIndex = 4;
+            // 
+            // log_reg
+            // 
+            this.log_reg.Location = new System.Drawing.Point(52, 98);
+            this.log_reg.Name = "log_reg";
+            this.log_reg.Size = new System.Drawing.Size(100, 20);
+            this.log_reg.TabIndex = 3;
+            // 
+            // btnRegister
+            // 
+            this.btnRegister.Location = new System.Drawing.Point(142, 225);
+            this.btnRegister.Name = "btnRegister";
+            this.btnRegister.Size = new System.Drawing.Size(145, 23);
+            this.btnRegister.TabIndex = 2;
+            this.btnRegister.Text = "зарегистрироваться";
+            this.btnRegister.UseVisualStyleBackColor = true;
+            btnRegister.Click += btnRegister_Click;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.label2.Location = new System.Drawing.Point(49, 65);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Логин:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(164, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(72, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Регистрация";
+            // 
+            // RegisterForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel1);
+            this.Name = "RegisterForm";
+            this.Text = "Окно регистрации";
+            this.Load += new System.EventHandler(this.RegisterForm_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox pass_reg;
+        private System.Windows.Forms.TextBox log_reg;
+        private System.Windows.Forms.Button btnRegister;
+    }
+}
